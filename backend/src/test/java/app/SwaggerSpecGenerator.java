56fd33a020f37909b6c6596e5bc7c82600b67442
chibat <tomofumi.chiba@gmail.com>
2017-02-24 07:33:55 +0900

    -

--- backend/src/test/java/app/SwaggerSpecGenerator.java
+++ backend/src/test/java/app/SwaggerSpecGenerator.java
@@ -7,19 +7,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import springfox.documentation.staticdocs.SwaggerResultHandler;
 
-@WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class)
 public class SwaggerSpecGenerator {
 
     @Autowired
