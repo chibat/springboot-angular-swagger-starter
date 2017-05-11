4a2ee28e5a806bf3f5a1493fb1319796dce374c1
chibat <tomofumi.chiba@gmail.com>
2017-05-12 00:34:09 +0900

    -

--- backend/src/main/java/app/controller/CalculatorController.java
+++ backend/src/main/java/app/controller/CalculatorController.java
@@ -1,4 +1,4 @@
-package app.web;
+package app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- backend/src/main/java/app/controller/ForwardController.java
+++ backend/src/main/java/app/controller/ForwardController.java
@@ -1,4 +1,4 @@
-package app.web;
+package app.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
